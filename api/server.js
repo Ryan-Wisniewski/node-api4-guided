fe0519478986401ec7a470076a42ecc674c048f9
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-09-05 13:32:29 -0400

    added start script

--- api/server.js
+++ api/server.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 
-
-const db = require('../data/db')
-db.connect({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS
-})
-
 console.log('environment', process.env.NODE_ENV)
 
 const apiRouter = require('./api-router.js');
